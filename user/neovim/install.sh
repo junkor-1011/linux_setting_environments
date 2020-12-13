3ed9e7858891df3f6d007898665b16311f120287
junkor-1011 <tomoyukizxy@gmail.com>
2020-12-13 18:55:22 +0900

    neovim

---
+++ user/neovim/install.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+# get script path
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+cd $SCRIPT_DIR
+
+# ToDo: Arguments
+VERSION=v0.4.4
+OS=linux64
+INSTALL_PATH=${HOME}/LocalApps
+EXECTIVE_PATH=${HOME}/bin
+
+NVIM_PACKAGE=nvim-${OS}
+DOWNLOAD_URL=https://github.com/neovim/neovim/releases/download/${VERSION}/${NVIM_PACKAGE}.tar.gz
+
+# download
+curl -LO ${DOWNLOAD_URL}
+
+# install
+tar zxvf ${NVIM_PACKAGE}.tar.gz     # inflate
+if [ ! -d ${INSTALL_PATH} ]; then
+    mkdir -p ${INSTALL_PATH}
+fi
+mv ${NVIM_PACKAGE} -t ${INSTALL_PATH}
+
+# remove trash
+rm ${NVIM_PACKAGE}.tar.gz
+
+# make synbolic link
+ln -s ${INSTALL_PATH}/${NVIM_PACKAGE}/bin/nvim ${EXECTIVE_PATH}/nvim
