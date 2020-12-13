0d62734269ccacce6d52ca4fb22ce874ecf9f62c
junkor-1011 <tomoyukizxy@gmail.com>
2020-12-13 14:52:32 +0000

    modify docker install (ubuntu)

--- root/docker/ubuntu/install.sh
+++ root/docker/ubuntu/install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+# execute by root
 
 # ref(docker): https://docs.docker.com/engine/install/ubuntu/
 # ref(compose): https://docs.docker.com/compose/install/
@@ -10,9 +11,9 @@
 VERSION_COMPOSE=1.27.4
 
 # setup the repository
-sudo apt-get update
+apt-get update
 
-sudo apt-get -y install \
+apt-get -y install \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -21,21 +22,24 @@ sudo apt-get -y install \
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-sudo apt-key -y fingerprint 0EBFCD88
+apt-key -y fingerprint 0EBFCD88
 
 # x86_64
-sudo add-apt-repository \
+add-apt-repository \
     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) \
     stable"
 
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io
 
 
 # docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/${VERSION_COMPOSE}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/${VERSION_COMPOSE}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 # or setting visudo to execute /usr/local/bin
-sudo ln -s /usr/bin/docker-compose /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+# service start
+service docker start 
