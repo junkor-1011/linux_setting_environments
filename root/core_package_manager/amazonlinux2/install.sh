99c225f03e6a95165709faec5fcd7b59da6b7bdb
test-user <test@email.com>
2020-12-23 00:00:29 +0900

    update packages

--- root/core_package_manager/amazonlinux2/install.sh
+++ root/core_package_manager/amazonlinux2/install.sh
@@ -3,6 +3,9 @@
 # root user execution
 set -eu
 
+# upgrade
+# yum update -y     # if need
+
 # epel
 amazon-linux-extras install epel -y
 yum-config-manager --enable epel
@@ -14,12 +17,24 @@ yum groupinstall "Development Tools"
 yum install -y \
     curl file git \
     expect \
-    bash zsh fish \
+    locales \
+    fontconfig \
+    tig subversion \
+    nkf dos2unix \
+    tree \
+    jq \
+    net-tools \
+    openssl openssl-devel \
+    tmux bash zsh fish \
     ruby \
     python3 python3-devel \
     the_silver_searcher ack \
-    unzip unrar p7zip \
+    unzip unrar p7zip xz-devel bzip2-devel \
     xclip
+yum install -y \
+    ctags
+    # global    # install failed
+yum autoremove -y
 
 # ripgrep
 yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
--- root/core_package_manager/ubuntu/install.sh
+++ root/core_package_manager/ubuntu/install.sh
@@ -9,26 +9,34 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 cd $SCRIPT_DIR
 
 # install core packages
-apt -y update
-apt install -y --no-install-recommends \
+apt-get -y update
+# apt -y upgrade
+apt-get install -y --no-install-recommends \
     build-essential \
-    zip unzip bzip2 \
+    zip unzip bzip2 xz-utils \
     p7zip-full unrar \
     nkf dos2unix \
+    tree \
+    jq \
+    net-tools \
     openssl libssl-dev \
     curl wget file \
-    git \
+    git tig subversion \
     ca-certificates \
     fontconfig \
     locales \
     expect \
     tmux bash zsh fish \
     sudo \
+    ruby \
     python3 python3-dev \
     silversearcher-ag ack \
     peco \
     global exuberant-ctags \
-    xclip
+    xclip \
+    pandoc
+apt-get -y upgrade
+apt-get -y autoremove
 
 # ripgrep
 sh additional/install_ripgrep_deb.sh
