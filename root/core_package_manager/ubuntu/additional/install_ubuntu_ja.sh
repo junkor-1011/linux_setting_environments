af193b7d701402e65943ed922945535f82ec5fa5
test-user <test@email.com>
2020-12-23 22:14:51 +0900

    modify japanese-setting

--- root/core_package_manager/ubuntu/additional/install_ubuntu_ja.sh
+++ root/core_package_manager/ubuntu/additional/install_ubuntu_ja.sh
@@ -15,12 +15,12 @@ if [ $UBUNTU_VERSION = "20.10" ]; then
     wget https://www.ubuntulinux.jp/ubuntu-jp-ppa-keyring.gpg -P /etc/apt/trusted.gpg.d/
     wget https://www.ubuntulinux.jp/ubuntu-ja-archive-keyring.gpg -P /etc/apt/trusted.gpg.d/
     wget https://www.ubuntulinux.jp/sources.list.d/groovy.list -O /etc/apt/sources.list.d/ubuntu-ja.list
-else if [ $UBUNTU_VERSION = "20.04" ]; then
+elif [ $UBUNTU_VERSION = "20.04" ]; then
     echo $UBUNTU_CODENAME
     wget -q https://www.ubuntulinux.jp/ubuntu-ja-archive-keyring.gpg -O- | apt-key add -
     wget -q https://www.ubuntulinux.jp/ubuntu-jp-ppa-keyring.gpg -O- | apt-key add -
     wget https://www.ubuntulinux.jp/sources.list.d/focal.list -O /etc/apt/sources.list.d/ubuntu-ja.list
-else if [ $UBUNTU_VERSION = "18.04" ]; then
+elif [ $UBUNTU_VERSION = "18.04" ]; then
     echo $UBUNTU_CODENAME
     wget -q https://www.ubuntulinux.jp/ubuntu-ja-archive-keyring.gpg -O- | apt-key add -
     wget -q https://www.ubuntulinux.jp/ubuntu-jp-ppa-keyring.gpg -O- | apt-key add -
