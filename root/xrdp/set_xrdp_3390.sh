f18f37fd51e613962fb8f17f5b0824c9018c850d
junkor-1011 <tomoyukizxy@gmail.com>
2020-12-27 12:11:06 +0900

    wip: xrdp for wsl2

--- root/xrdp/set_xrdp_3390.sh
+++ root/xrdp/set_xrdp_3390.sh
@@ -12,6 +12,8 @@ cp /etc/xrdp/xrdp.ini /etc/xrdp/xrdp.ini.bk_$(date "+%Y-%m-%dT%H:%M:%S")
 # change port 3389(default) -> 3390
 # (in wsl2, 3389 is not available for xrdp...)
 sed -i 's/3389/3390/g' /etc/xrdp/xrdp.ini
+# cursor
+sed -i 's/new_cursors=true/new_cursors=false/g' /etc/xrdp/xrdp.ini
 # color setting
 sed -i 's/max_bpp=32/#max_bpp=32\nmax_bpp=128/g' /etc/xrdp/xrdp.ini
 sed -i 's/xserverbpp=24/#xserverbpp=24\nxserverbpp=128/g' /etc/xrdp/xrdp.ini
@@ -19,5 +21,11 @@ sed -i 's/xserverbpp=24/#xserverbpp=24\nxserverbpp=128/g' /etc/xrdp/xrdp.ini
 # service
 service xrdp start
 
+cat <<EOL > ~/.xsessionrc
+export GNOME_SHELL_SESSION_MODE=ubuntu
+export XDG_CURRENT_DESKTOP=ubuntu:GNOME
+export XDG_DATA_DIRS=/usr/share/ubuntu:/usr/local/share:/usr/share:/var/lib/snapd/desktop
+export XDG_CONFIG_DIRS=/etc/xdg/xdg-ubuntu:/etc/xdg
+EOL
 
 echo "INFO: finished xrdp-setting for wsl2"
