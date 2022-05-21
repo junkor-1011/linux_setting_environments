3a1a32e4db7bc5b634c2aaa3d9be2fcd9aa95a65
junkor-1011 <tomoyukizxy@gmail.com>
2022-05-21 16:06:58 +0900

    change: conda env for pynvim

--- user/anyenv/post_install.sh
+++ user/anyenv/post_install.sh
@@ -3,7 +3,7 @@
 
 # ToDo: Arguments Control
 # NODEJS_VERSION=14.17.6
-NODEJS_VERSION=16.9.1
+NODEJS_VERSION=16.15.0
 
 # pyenv
 
@@ -14,11 +14,11 @@ NODEJS_VERSION=16.9.1
 pyenv install mambaforge
 
 # neovim setting
-curl -LO https://gist.githubusercontent.com/junkor-1011/b7a8e5dc1f6d633fa589635c04ae3785/raw/2e0452c4c70b8b4180fd381c990f034aa821728c/create_env_nvim.yml
+# curl -LO https://gist.githubusercontent.com/junkor-1011/b7a8e5dc1f6d633fa589635c04ae3785/raw/2e0452c4c70b8b4180fd381c990f034aa821728c/create_env_nvim.yml
 # pyenv shell miniconda3-latest
-CONDA=$(pyenv root)/versions/mambaforge/bin/conda
-$CONDA env create -f create_env_nvim.yml
-rm create_env_nvim.yml
+# CONDA=$(pyenv root)/versions/mambaforge/bin/conda
+# $CONDA env create -f create_env_nvim.yml
+# rm create_env_nvim.yml
 
 
 # node setting
@@ -27,6 +27,6 @@ nodenv global ${NODEJS_VERSION}
 npm i -g yarn
 npm i -g serve
 npm i -g neovim
-npm i -g htmlhint eslint eslint_d prettier stylelint eslint-config-prettier eslint-plugin-prettier prettier-eslint-cli
-npm i -g typescript typescript-language-server
+# npm i -g htmlhint eslint eslint_d prettier stylelint eslint-config-prettier eslint-plugin-prettier prettier-eslint-cli
+npm i -g typescript typescript-language-server eslint-language-server
 # npm i -g create-react-app
