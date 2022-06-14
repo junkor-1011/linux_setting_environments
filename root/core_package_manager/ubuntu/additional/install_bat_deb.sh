215859534b57880c94058053f5c10ea5ae1fb07a
junkor-1011 <tomoyukizxy@gmail.com>
2022-06-14 23:27:20 +0900

    chore: update package

--- root/core_package_manager/ubuntu/additional/install_bat_deb.sh
+++ root/core_package_manager/ubuntu/additional/install_bat_deb.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION=0.17.1
+VERSION=0.21.0
 FILENAME_DEB=bat-musl_${VERSION}_amd64.deb
 DOWNLOAD_URL=https://github.com/sharkdp/bat/releases/download/v${VERSION}/${FILENAME_DEB}
 
