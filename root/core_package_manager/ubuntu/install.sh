9e1f67f4b4e44a2ddaf913eefa944de8d4b2a4d4
junkor-1011 <tomoyukizxy@gmail.com>
2022-06-14 23:46:31 +0900

    feat: install hexyl fd-find

--- root/core_package_manager/ubuntu/install.sh
+++ root/core_package_manager/ubuntu/install.sh
@@ -36,7 +36,7 @@ apt-get install -y --no-install-recommends \
     python3 python3-dev python3-pip python3-venv \
     silversearcher-ag ack \
     peco \
-    exa \
+    exa hexyl fd-find \
     global exuberant-ctags \
     xclip x11-apps mesa-utils-extra \
     pandoc \
