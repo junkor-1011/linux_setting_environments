a070a1afe45108cfde8c6aefa6297e24e6ee6633
junkor-1011 <tomoyukizxy@gmail.com>
2022-07-02 20:57:04 +0900

    style: shfmt & shellcheck

--- user/anyenv/install.sh
+++ user/anyenv/install.sh
@@ -2,49 +2,55 @@
 set -eu
 
 # get script path
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
-cd $SCRIPT_DIR
+SCRIPT_DIR=$(
+    cd "$(dirname "$0")"
+    pwd
+)
+cd "$SCRIPT_DIR"
 
 # Clone anyenv
-if [ ! -d ${HOME}/.anyenv ]; then
-    git clone https://github.com/anyenv/anyenv ${HOME}/.anyenv
+if [ ! -d "${HOME}/.anyenv" ]; then
+    git clone https://github.com/anyenv/anyenv "${HOME}/.anyenv"
 else
     # echo "ERROR: anyenv already exists."
     # exit 1
-    cd ${HOME}/.anyenv; git pull; cd -
+    cd "${HOME}/.anyenv"
+    git pull
+    cd -
 fi
 
 # initialize for setup
 export PATH="$HOME/.anyenv/bin:$PATH"
-if [ ! -d ${HOME}/.config/anyenv ]; then
-    mkdir -p ${HOME}/.config/anyenv
+if [ ! -d "${HOME}/.config/anyenv" ]; then
+    mkdir -p "${HOME}/.config/anyenv"
 fi
-if [ -d ${HOME}/.config/anyenv/anyenv-init ]; then
-    rm -rf ${HOME}/.config/anyenv/anyenv-init
+if [ -d "${HOME}/.config/anyenv/anyenv-init" ]; then
+    rm -rf "${HOME}/.config/anyenv/anyenv-init"
 fi
-if [ ! -d ${HOME}/.config/anyenv/anyenv-install ]; then
-    git clone https://github.com/anyenv/anyenv-install.git ${HOME}/.config/anyenv/anyenv-install
+if [ ! -d "${HOME}/.config/anyenv/anyenv-install" ]; then
+    git clone https://github.com/anyenv/anyenv-install.git "${HOME}/.config/anyenv/anyenv-install"
 else
-    cd ${HOME}/.config/anyenv/anyenv-install; git pull; cd -
+    cd "${HOME}/.config/anyenv/anyenv-install"
+    git pull
+    cd -
 fi
 # anyenv install --init
 eval "$(anyenv init -)"
 
 # install anyenv plugins
-mkdir -p $(anyenv root)/plugins
-git clone https://github.com/znz/anyenv-update.git $(anyenv root)/plugins/anyenv-update
-git clone https://github.com/znz/anyenv-git.git $(anyenv root)/plugins/anyenv-git
+mkdir -p "$(anyenv root)/plugins"
+git clone https://github.com/znz/anyenv-update.git "$(anyenv root)/plugins/anyenv-update"
+git clone https://github.com/znz/anyenv-git.git "$(anyenv root)/plugins/anyenv-git"
 
 # install pyenv
 anyenv install pyenv
 # PYENV=${HOME}/.anyenv/envs/pyenv/bin/pyenv
 # eval "$($PYENV init -)"
-# 
 # echo $($PYENV root)
 
 # install pyenv plugins
 PYENV_ROOT=${HOME}/.anyenv/envs/pyenv
-git clone https://github.com/pyenv/pyenv-virtualenv.git ${PYENV_ROOT}/plugins/pyenv-virtualenv
+git clone https://github.com/pyenv/pyenv-virtualenv.git "${PYENV_ROOT}/plugins/pyenv-virtualenv"
 
 # install miniconda3-latest
 # ToDo: Arguments Control
@@ -54,8 +60,6 @@ git clone https://github.com/pyenv/pyenv-virtualenv.git ${PYENV_ROOT}/plugins/py
 # ${PYENV} shell miniconda3-latest
 # conda env create -f create_env_nvim.yml
 # rm create_env_nvim.yml
-
-
 
 # install nodenv
 anyenv install nodenv
