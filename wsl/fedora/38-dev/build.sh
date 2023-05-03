085f60b6c0a2149b54d823dc051287bc0dd30496
junkor-1011 <tomoyukizxy@gmail.com>
2023-05-03 17:25:37 +0900

    wsl2: git prompt & completion(WIP)

--- wsl/fedora/38-dev/build.sh
+++ wsl/fedora/38-dev/build.sh
@@ -6,6 +6,11 @@ TAG="fedora38-dev"
 SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd)"
 cd "$SCRIPT_DIR"
 
+# git-prompt & git-completion
+curl -o ./git-completion/git-prompt.sh -L https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+curl -o ./git-completion/git-completion.bash -L https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+curl -o ./git-completion/git-completion.zsh -L https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
+
 # tmux
 cp ../../../dotfiles/tmux/.tmux.conf .
 
