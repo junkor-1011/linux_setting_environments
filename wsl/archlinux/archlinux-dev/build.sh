84528950d0fab635b15a41d27af115ddab72f448
junkor-1011 <tomoyukizxy@gmail.com>
2023-05-03 16:11:06 +0900

    wsl2: add tmux conf & switch builder(docker or podman)

--- wsl/archlinux/archlinux-dev/build.sh
+++ wsl/archlinux/archlinux-dev/build.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 set -eu
 
+TAG="archlinux-dev"
+
 SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd)"
 cd "$SCRIPT_DIR"
 
-docker build -t archlinux-dev .
+# tmux
+cp ../../../dotfiles/tmux/.tmux.conf .
+
+if [ "$1" = "podman" ]; then
+    podman build -t "$TAG" .
+else
+    docker build -t "$TAG" .
+fi
--- wsl/fedora/38-dev/build.sh
+++ wsl/fedora/38-dev/build.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 set -eu
 
+TAG="fedora38-dev"
+
 SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd)"
 cd "$SCRIPT_DIR"
 
-docker build -t fedora38-dev .
+# tmux
+cp ../../../dotfiles/tmux/.tmux.conf .
+
+if [ "$1" = "podman" ]; then
+    podman build -t "$TAG" .
+else
+    docker build -t "$TAG" .
+fi
