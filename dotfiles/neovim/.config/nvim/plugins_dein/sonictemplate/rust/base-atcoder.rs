d53da68d69b7a6440cf342d32e4faf47dcd8ddb3
junkor-1011 <tomoyukizxy@gmail.com>
2023-08-09 22:27:15 +0900

    feat(nvim): add snippet for rust(atcoder)

---
+++ dotfiles/neovim/.config/nvim/plugins_dein/sonictemplate/rust/base-atcoder.rs
@@ -0,0 +1,25 @@
+#![allow(non_snake_case)]
+use proconio::input;
+
+// use std::*;
+
+// use itertools::Itertools;
+// use petgraph::unionfind::UnionFind;
+// use superslice::*
+// see more: https://github.com/rust-lang-ja/atcoder-rust-base/tree/ja-all-enabled
+
+// const MOD: i32 = 1_000_000_007;
+
+fn main() {
+    {{_cursor_}}
+    input! {
+        a: i128,
+        b: f64,
+        m: usize,
+        n: usize,
+        s: String,
+        arr: [[i32; n]; m],
+    }
+
+    println!("{}", s);
+}
