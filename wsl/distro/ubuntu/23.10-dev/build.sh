d8783af065813802955eb3e67eb79e539919ca3a
junkor-1011 <tomoyukizxy@gmail.com>
2024-04-10 22:27:27 +0900

    fix(wsl2): path

--- wsl/distro/ubuntu/23.10-dev/build.sh
+++ wsl/distro/ubuntu/23.10-dev/build.sh
@@ -18,7 +18,7 @@ if [ ! -f ./git-completion/git-completion.zsh ]; then
 fi
 
 # tmux
-cp ../../../dotfiles/tmux/.tmux.conf .
+cp ../../../../dotfiles/tmux/.tmux.conf .
 
 if [ "$1" = "podman" ]; then
     podman build -t "$TAG" .
